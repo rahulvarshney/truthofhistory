70a512aa440c7240cb8260b276d38232d9583885
rahulvarshney <rahulvarshney@gmail.com>
2024-10-04 12:11:55 -0700

    0.1.1 cheerio

--- scraper.ts
+++ scraper.ts
@@ -1,10 +1,15 @@
+import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+
 const url = 'https://example.com';
 
 try {
   const res = await fetch(url);
   const html = await res.text();
+  const $ = cheerio.load(html)  
 
-  console.log(html)
+  const pageHeader = $('h1').text();
+
+  console.log(pageHeader)
 } catch(error) {
   console.log(error);
 }
